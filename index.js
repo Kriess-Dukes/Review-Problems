c83b90ae8c72c10fd376d04391ba9b7561d33861
Kriess Dukes <kdukes282@noscihigh.org>
2021-03-17 01:00:57 +0000

    I changed some codes and added codes.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 ///Strings
 //First, declare variable named firstName.
-
+var firstName;
 //Assign firstName to a string value of your first name.
-
+firstName = 'Sasuke'
 //Declare another variable named lastName and assign it the value of your last name.
-
+var lastName= 'Uzimaki'
 //Then declare another variable named fullName and assign it to the current value of firstName and lastName and have the joined values separated with a space character.
-
+var fullName = firstName + ' ' + lastName
+console.log(fullName)
 //Remember that computer languages will not add the space character for you so how would you do that manually?
 
 
@@ -19,11 +20,11 @@
 
 //Numbers
 //Declare a variable named luckyNumber.
-
+var luckyNumber
 //Assign luckyNumber to a number of your choice.
-
+var luckyNukber = 10
 //Declare another variable named luckyNumberTimes2 and assign it the value of luckyNumber multiplied by 2.
-
+var luckyNumberTimes2 = luckyNumber * 2;
 
 
 
@@ -36,10 +37,10 @@
 
 //Booleans
 //Declare a variable named myBool and assign it the value of true.
-
+var myBool = true
 
 //Then, declare another named isEqualTo and assign it the value of strictly comparing luckyNumber and luckyNumberTimes2.
-
+var isEqualTo = luckyNumber === luckyNumberTimes2
 
 
 
@@ -67,17 +68,19 @@ factorial(8); // => 40320 (8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
 
 function factorial(number){
 //CODE BELOW HERE
-function factorial(n) {
-  return (n != 1) ? n * factorial(n - 1) : 1;
+var answer = 1;
+
+for(var i = number; i >= 1; i--){
+
+  answer = answer * i;
+
+  console.log(answer)
 }
-
-alert( factorial(9) ); // 362880
-
 
 //CODE ABOVE HERE
 }
 
-
+factorial(10)
 
 
 
@@ -93,10 +96,10 @@ reverseString('Hello, World!'); // => '!dlroW ,olleH'
 
 function reverseString(string){
   //CODE BELOW HERE
- function reverseString('Naruto Drip Sauce, No Cap') 
+  reverseString = ('Naruto Drip Sauce, No Cap') 
  return ('paC oN, ecuaS pirD oturaN)
  
- console log(reverseString)
+ console log(return)
   //CODE ABOVE HERE
 }
 
